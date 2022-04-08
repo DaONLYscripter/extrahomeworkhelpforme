a07a983e34661f7ad1edf2126df924fc4c6d997b
yungryzen <>
2022-04-08 14:54:42 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//This is to open the menu
+function openNav() {
+			document.getElementById("sidenav").style.width="300px";
+		}
+
+//This is to close the menu
+function closeNav() {
+			document.getElementById("sidenav").style.width="0";
+		}
+
+ 
+
+    
